c17233dd9658c3c8119365251217e8ce4357bd96
jara <juanignaciojara505@gmail.com>
2025-06-27 00:25:26 -0600

    second first commit

--- pyramscope/setup.py
+++ pyramscope/setup.py
@@ -10,12 +10,22 @@ ext_modules = [
         extra_compile_args=["-std=c++17"],
     ),
 ]
-
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+   
+ 
 setup(
     name="pyramscope",
     version="0.1.0",
     packages=["pyramscope"],
     ext_modules=ext_modules,
-    author="TuNombre",
-    description="Pequeña librería para inspeccionar objetos Python con C++",
+    author="Juan Jara",
+    author_email="juanignaciojara505@gmail.com",
+    description="Small Python library to inspect Python objects using C++",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    ext_modules=ext_modules,
+    python_requires=">=3.7",
+    zip_safe=False,
 )
