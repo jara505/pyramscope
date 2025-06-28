f33ce1ba83c65f0ec14782bfeaf144e515d3d928
jara <juanignaciojara505@gmail.com>
2025-06-27 19:31:01 -0600

    update setup.py

--- pyramscope/setup.py
+++ pyramscope/setup.py
@@ -1,35 +1,29 @@
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 import pybind11
-import os
-
-# Ruta al README
-this_directory = os.path.abspath(os.path.dirname(__file__))
-readme_path = os.path.join(this_directory, "README.md")
-
-with open(readme_path, "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
 ext_modules = [
     Extension(
         "_pyramscope",
-        ["core.cpp"],
+        ["pyramscope/core.cpp"],
         include_dirs=[pybind11.get_include()],
         language="c++",
         extra_compile_args=["-std=c++17"],
     ),
 ]
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="pyramscope",
     version="0.1.0",
-    packages=["."],  # punto porque está todo en la misma carpeta
-    ext_modules=ext_modules,
-    author="Juan Jara 🇳🇮",
+    author="Juan Jara",
     author_email="juanignaciojara505@gmail.com",
     description="Small Python library to inspect Python objects using C++",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    packages=find_packages(),  # Busca automáticamente paquetes
+    ext_modules=ext_modules,
     python_requires=">=3.7",
-    license="MIT",
     zip_safe=False,
 )
